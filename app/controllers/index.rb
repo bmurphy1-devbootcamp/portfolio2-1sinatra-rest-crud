367b8548160ce49b23dc565caa59dd0a3a248eb6
Brian Murphy <bmurphy1@mac.com>
2014-02-05 19:14:01 -0600

    Add note model and migration

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,28 @@
 get '/' do
-  # Look in app/views/index.erb
+  @posts = Post.all
   erb :index
+end
+
+get '/posts' do
+  erb :posts
+end
+
+get '/posts/new' do
+  erb :posts_new
+end
+
+post '/posts' do
+end
+
+get '/posts/:id' do
+end
+
+get '/posts/edit/:id' do
+  erb :posts_edit
+end
+
+put '/posts/:id' do
+end
+
+delete '/photos/:id' do
 end
